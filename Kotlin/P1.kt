07689ebdad3d78abce8da36eb93e7885719c16da
Devesh Pandey <deveshp1001@gmail.com>
2022-09-12 19:02:04 +0530

    added code files

---
+++ Kotlin/P1.kt
@@ -0,0 +1,11 @@
+package alpha.beta.android_fundamentals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
